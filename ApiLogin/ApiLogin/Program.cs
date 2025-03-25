f831c81f3f3e7acf685b84d6289c2be3c679a3b2
Ederam <53803895+Ederam@users.noreply.github.com>
2025-03-25 14:31:55 -0500

    Configuracion context
    Se crea la clase para uso del contexto de base de datos, se realiza configuracion en program para poder utilizar el context

--- ApiLogin/ApiLogin/Program.cs
+++ ApiLogin/ApiLogin/Program.cs
@@ -1,3 +1,5 @@
+using ApiLogin.Context;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,9 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+//uso de la clase de context para poder utilizarla en la aplicacion
+builder.Services.AddDbContext<AppDbContext>(options => builder.Configuration.GetConnectionString("ConexionBD"));
 
 var app = builder.Build();
 

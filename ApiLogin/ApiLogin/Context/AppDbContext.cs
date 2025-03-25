07e754fba143159c31f7ab7a49d4ddebabdd58a3
Ederam <53803895+Ederam@users.noreply.github.com>
2025-03-25 12:06:11 -0500

    Agregar Context
    Se crea clase para contexto, se agrega package de entityFramework

--- ApiLogin/ApiLogin/Context/AppDbContext.cs
+++ ApiLogin/ApiLogin/Context/AppDbContext.cs
@@ -1,6 +1,15 @@
-﻿namespace ApiLogin.Context
+﻿using ApiLogin.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace ApiLogin.Context
 {
-    public class AppDbContext
+    public class AppDbContext : DbContext
     {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Usuarios> usuarios { get; set; }
     }
 }
--- ApiLogin/ApiLogin/Models/Usuarios.cs
+++ ApiLogin/ApiLogin/Models/Usuarios.cs
@@ -1,6 +1,19 @@
-﻿namespace ApiLogin.Models
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace ApiLogin.Models
 {
+    [Table("UsuariosReact")]
     public class Usuarios
     {
+        [Key]
+        public int id { get; set; }
+        public string  apellido_paterno { get; set; }
+        public string  apellido_materno { get; set; }
+        public string  nombre { get; set; }
+        public string  correo { get; set; }
+        public string  username { get; set; }
+        public string  password { get; set; }
+        
     }
 }
